fa5c77f0d58e821b7e2ad0345a8c469fc9990ae3
Yuuto Yoshimura <0615yutoyoshimu@gmail.com>
2019-12-04 20:30:16 +0900

    snsデザイン完了

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,22 @@
 //= require jquery/dist/jquery.js
 //= require_tree .
 
+
+// 投稿ボタン
+var animateButton = function(e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}

f20583cca9b623eab8633eba98899e45c8ffa348
Yuuto Yoshimura <0615yutoyoshimu@gmail.com>
2019-12-09 20:23:54 +0900

    callbackurl指定

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,7 @@ Devise.setup do |config|
   config.secret_key = 'd1da80f056dcb11f61c9938119185cff5c4ef4d29239db34d2f343c51013e5192032ecd35ab2c57881b4edd19c74ba501a2ad02ce7fb39b1682b944310e67a8b'
   config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'], name: :google, skip_jwt: true
   config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
-  config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
+  config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'], callback_url: "https://safe-headland-35363.herokuapp.com/users/auth/twitter/callback"
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'

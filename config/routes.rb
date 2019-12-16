8b23096191c04f1aa825ae40f11fa02f4f4253b9
Yuuto Yoshimura <0615yutoyoshimu@gmail.com>
2019-12-16 23:24:11 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,14 @@
 Rails.application.routes.draw do
-  get 'families/index'
-  resources :philosophies
   resources :articles
-  resources :families
   root 'tops#index'
-  get "/shizuka", to: "devise/mailer#confirmation_instructions"
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
-  resources :moneis
   resources :shares do
     resources :comments
   end
-  get "/users", to: "users#index"
-  get "/articles/loves", to: "articles#love"
-
 
   resources :users, only: [:index, :show]
   resources :conversations do

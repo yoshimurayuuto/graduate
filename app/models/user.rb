e850be13f5bb4400fe54cb636927d4bc62a8b53f
Yuuto Yoshimura <0615yutoyoshimu@gmail.com>
2019-12-17 14:29:36 +0900

    Userモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
   validates :username,  presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-   before_validation { email.downcase! }
   has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed

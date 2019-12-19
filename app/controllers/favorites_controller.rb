d83fbd952f1268bb4397351d195cd54f4b370644
Yuuto Yoshimura <0615yutoyoshimu@gmail.com>
2019-12-19 10:55:53 +0900

    デプロイ

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,7 +7,12 @@ class FavoritesController < ApplicationController
 
   def create
     favorite = current_user.favorites.create(article_id: params[:article_id])
-    redirect_to articles_url, notice: "#{favorite.article.user.name}さんの記事をお気に入り登録しました"
+    if favorite.article.user.name.present?
+      redirect_to articles_url, notice: "#{favorite.article.user.name}さんの記事をお気に入り登録しました"
+    else
+      redirect_to articles_url, notice: "#{favorite.article.user.username}さんの記事をお気に入り登録しました"
+    end
+
   end
 
   def destroy

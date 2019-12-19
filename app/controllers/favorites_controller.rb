9b2937fd8d0f370fd9ba27f7420b874569bbc930
Yuuto Yoshimura <0615yutoyoshimu@gmail.com>
2019-12-19 11:09:17 +0900

    試しデプロイ

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -17,6 +17,11 @@ class FavoritesController < ApplicationController
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to articles_url, notice: "#{favorite.article.user.name}さんの記事をお気に入り解除しました"
+    if favorite.article.user.name.present?
+      redirect_to articles_url, notice: "#{favorite.article.user.name}さんの記事をお気に入り解除しました"
+    else
+      redirect_to articles_url, notice: "#{favorite.article.user.username}さんの記事をお気に入り解除しました"
+    end
+
   end
 end
